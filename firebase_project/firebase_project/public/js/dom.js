c0d2e85f68340aa09ef21f471172c9f456a08f96
efsi2 <129748068+efsi2@users.noreply.github.com>
2023-10-11 08:16:23 -0300

    rgrg

--- firebase_project/firebase_project/public/js/dom.js
+++ firebase_project/firebase_project/public/js/dom.js
@@ -1,1 +1,16 @@
+function casilla(posicion) {
+    console.log(posicion)
+}
 
+function changeScreenadmin() {
+    const admin = document.getElementById("admin");
+    const login = document.getElementById("login");
+    if(admin.style.display !== "none") {
+        admin.style.display = "none";
+        login.style.display = "";
+    }
+    else {
+        admin.style.display = "";
+        login.style.display = "none";
+    }
+}
