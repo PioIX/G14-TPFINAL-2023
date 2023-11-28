ec2bdda5220564a0c4d7a5140c0bd2506e335755
JuaniSuleta <129747755+Uchechong410@users.noreply.github.com>
2023-11-28 11:11:48 -0300

    28/11

--- firebase_project/firebase_project/public/js/WebSocket.js
+++ firebase_project/firebase_project/public/js/WebSocket.js
@@ -15,23 +15,40 @@ function funcionPrueba() {
     socket.emit("mensaje-prueba",{mensaje: "hola"})
 }
 
-function unirseprep() {
-  socket.emit("unirme-prep",{mensaje:"Jugando devuelta"} )
-  location.href = '/prep'
-}
+// function unirseprep() {
+//   socket.emit("unirme-prep",{mensaje:"Jugando devuelta"} )
+//   location.href = '/prep'
+// }
 
+function reslogin(){
+  location.href= '/login'
+}
 
 function unirseSala() {
     socket.emit('unirme-sala',{mensaje:"uniendose a la sala"} )
     location.href = '/ata'
 }
 
-socket.on("verAtaque", data =>
-    alert(data.mensaje)
-)
-socket.on("dioagua", data =>
-    alert(data.mensaje)
-)
+socket.on("verAtaque", data =>{
+  alert(data.mensaje);
+  let turno = document.getElementById("turnos").innerHTML;
+    if (turno == 1){
+      document.getElementById("turnos").innerHTML = "2"
+    } else if (turno == 2){
+      document.getElementById("turnos").innerHTML = "1"
+    }
+  console.log(turno)
+});
+socket.on("dioagua", data =>{
+  alert(data.mensaje)
+  let turno = document.getElementById("turnos").innerHTML;
+    if (turno == 1){
+      document.getElementById("turnos").innerHTML = "2"
+    } else if (turno == 2){
+      document.getElementById("turnos").innerHTML = "1"
+    }
+  console.log(turno)
+});
 
 socket.on("final", data =>{
   console.log("termino")
@@ -43,16 +60,15 @@ var barcosAtacados = [];
 function ataque(posicion){
     let posicionatacada = document.getElementById(posicion.id)
     let jugador = document.getElementById("playerId").value;
-    if(jugador == "1")
-      jugador = "2";
-    else
-      jugador = "1";
-    let pego = false;
-    console.log("posicion atacada:",posicion.id)
-    var atacado = document.getElementById(baseBarcos.bdd[0].J2B1);
-    let posAtacada = posicionatacada.id;
-    posAtacada = posAtacada.replace("R", "");
-    for(let i = 1; i <= 4; i++) {
+    let turno = document.getElementById("turnos").innerHTML;
+    let pego = false
+    if (jugador==turno){
+    
+      console.log("posicion atacada:",posicion.id)
+      var atacado = document.getElementById(baseBarcos.bdd[0].J2B1);
+      let posAtacada = posicionatacada.id;
+      posAtacada = posAtacada.replace("R", "");
+      for(let i = 1; i <= 4; i++) {
       console.log(posAtacada == baseBarcos.bdd[0][`J${jugador}B${i}`].replace("P", ""), posAtacada, baseBarcos.bdd[0][`J${jugador}B${i}`].replace("P", ""))
       if(posAtacada == baseBarcos.bdd[0][`J${jugador}B${i}`].replace("P", "")) {
         // console.log("PEGUE", posicionatacada, baseBarcos.bdd[0][`J${jugador}B${i}`])
@@ -63,6 +79,7 @@ function ataque(posicion){
         socket.on('verAtaque', data =>{
           console.log("El usuario:", data.jugador, "dió fuego en:", data.lugar)
           render(data)
+          
         })  
         barcosAtacados.push(posAtacada)
         console.log(barcosAtacados)
@@ -70,20 +87,23 @@ function ataque(posicion){
           socket.emit("fin")
           finJuego();
         }
-        
         return;      
         
       }
+      }
+      if (pego == false) {
+          posicionatacada.classList.add("hitaguacasilla");
+          socket.emit("agua", {lugar: posicion.id, jugador: playerId.value})
+          socket.on('dioagua', data =>{
+            console.log("El usuario:", data.jugador, "dió agua en:", data.lugar)
+            render(data)
+            
+          }) 
+      }
+    }   
+    else
+    {console.log(" No es tu turno",turno,jugador)
     }
-    if (pego == false) {
-        posicionatacada.classList.add("hitaguacasilla");
-        socket.emit("agua", {lugar: posicion.id, jugador: playerId.value})
-        socket.on('dioagua', data =>{
-          console.log("El usuario:", data.jugador, "dió agua en:", data.lugar)
-          render(data)
-        }) 
-    }
-    
     
 }
 
@@ -92,9 +112,9 @@ function ataque(posicion){
 //movimiento mandar a la sala y a la base de datos
 function render(data){
   if (data.jugador == 1){
-    document.getElementById("turnos").innerHTML = "Es turno del jugador 2"
+    document.getElementById("turnos").innerHTML = "2"
   } else if (data.jugador == 2){
-    document.getElementById("turnos").innerHTML = "Es turno del jugador 1"
+    document.getElementById("turnos").innerHTML = "1"
   }
 
 }
